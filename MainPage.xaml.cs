3f953c4731aee738eedf6cf91d44c24228ab82ae
SCarrera18 <131091849+SCarrera18@users.noreply.github.com>
2023-11-27 20:24:22 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Final_DBP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> Level { get; set; }
+        public List<string> Languages { get; set; }
+        public List<string> Aptitudes { get; set; }
+        public List<string> Skills { get; set; }
+
+
+
+
+        private async void OnButtonClicked(System.Object sender, System.EventArgs e)
+        {
+            var name = name_.Text;
+            var dateofBirth = date_.Date;
+            var occupation = occupation_.Text;
+            var phone = phone_.Text;
+            var email = email_.Text;
+            var nationality = string.Empty;
+            nationality = nationality_.Items[nationality_.SelectedIndex];
+            var _level = "";
+            if (RadioBtnBasico.IsChecked) { _level = RadioBtnBasico.Value.ToString(); }
+            if (RadioBtnIntermedio.IsChecked) { _level = RadioBtnIntermedio.Value.ToString(); }
+            if (RadioBtnAvanzado.IsChecked) { _level = RadioBtnAvanzado.Value.ToString(); }
+            if (RadioBtnFluido.IsChecked) { _level = RadioBtnFluido.Value.ToString(); }
+            Level.Add(_level);
+            if (Cpp.IsChecked) { Languages.Add("C++"); }
+            if (Csharp.IsChecked) { Languages.Add("C#"); }
+            if (Java.IsChecked) { Languages.Add("Java"); }
+            if (Python.IsChecked) { Languages.Add("Python"); }
+            var aptitudes = string.Empty;
+            aptitudes = aptitudes_.Items[aptitudes_.SelectedIndex];
+            Aptitudes.Add(aptitudes);
+            if (skill_1.IsChecked) { Skills.Add("Acondicionamiento fisico"); }
+            if (skill_2.IsChecked) { Skills.Add("Creacion de videos deportivos"); }
+            if (skill_3.IsChecked) { Skills.Add("Pack office"); }
+            if (skill_4.IsChecked) { Skills.Add("Tenis"); }
+            var profile = profile_.Text;
+
+            List<string> RadioBtns = new List<string>();
+            var RadioBtns_ = levels_.Children.ToList();
+            foreach (var i in RadioBtns_)
+            {
+                var myEntry = i as Entry;
+                RadioBtns.Add(myEntry.Text);
+            }
+
+
+            await Application.Current.MainPage.Navigation.PushModalAsync(new Page1(name, dateofBirth, occupation, phone, email, nationality, Level, Languages, Aptitudes, Skills, profile), true);
+        }
+
+
+
+        void AddBtnEnglish(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter new languages" };
+            levels_.Children.Add(entry);
+        }
+        void AddBtnLanguages(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter new languages programming" };
+            entry = (Entry)languageslst.Children.Last();
+            Languages.Add(entry.Text);
+        }
+        void AddBtnAptitudes(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter new aptitudes" };
+            Aptitudes.Add(entry.Text);
+        }
+        void AddBtnSkills(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter new skills" };
+            entry = (Entry)skillslst.Children.Last();
+            Skills.Add(entry.Text);
+        }
+
+
+
+    }
+}
---
+++ Page1.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Final_DBP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private string nameContentProperty;
+        private DateTime dateContentProperty;
+        private string occupationContentProperty;
+        private string phoneContentProperty;
+        private string emailContentProperty;
+        private string nationalityContentProperty;
+        private List<string> levelContentProperty = new List<string>();
+        private List<string> languagesContentProperty = new List<string>();
+        private List<string> aptitudesContentProperty = new List<string>();
+        private List<string> skillsContentProperty = new List<string>();
+        private string profileContentProperty;
+
+        public string NameContentProperty
+        {
+            get { return nameContentProperty; }
+            set
+            {
+                nameContentProperty = value;
+                OnPropertyChanged(nameof(NameContentProperty));
+            }
+        }
+        public DateTime DateContentProperty
+        {
+            get { return dateContentProperty; }
+            set
+            {
+                dateContentProperty = value;
+                OnPropertyChanged(nameof(DateContentProperty));
+            }
+        }
+        public string OccupationContentProperty
+        {
+            get { return occupationContentProperty; }
+            set
+            {
+                occupationContentProperty = value;
+                OnPropertyChanged(nameof(OccupationContentProperty));
+            }
+        }
+        public string PhoneContentProperty
+        {
+            get { return phoneContentProperty; }
+            set
+            {
+                phoneContentProperty = value;
+                OnPropertyChanged(nameof(PhoneContentProperty));
+            }
+        }
+        public string EmailContentProperty
+        {
+            get { return emailContentProperty; }
+            set
+            {
+                emailContentProperty = value;
+                OnPropertyChanged(nameof(EmailContentProperty));
+            }
+        }
+        public string NationalityContentProperty
+        {
+            get { return nationalityContentProperty; }
+            set
+            {
+                nationalityContentProperty = value;
+                OnPropertyChanged(nameof(NationalityContentProperty));
+            }
+        }
+        public List<string> LevelContentProperty
+        {
+            get { return levelContentProperty; }
+            set
+            {
+                levelContentProperty = value;
+                OnPropertyChanged(nameof(LevelContentProperty));
+            }
+        }
+        public List<string> LanguagesContentProperty
+        {
+            get { return languagesContentProperty; }
+            set
+            {
+                if (value != null)
+                {
+                    languagesContentProperty = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public List<string> AptitudesContentProperty
+        {
+            get { return aptitudesContentProperty; }
+            set
+            {
+                aptitudesContentProperty = value;
+                OnPropertyChanged(nameof(AptitudesContentProperty));
+            }
+        }
+        public List<string> SkillsContentProperty
+        {
+            get { return skillsContentProperty; }
+            set
+            {
+                if (value != null)
+                {
+                    skillsContentProperty = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string ProfileContentProperty
+        {
+            get { return profileContentProperty; }
+            set
+            {
+                profileContentProperty = value;
+                OnPropertyChanged(nameof(ProfileContentProperty));
+            }
+        }
+
+        public Page1(string nameContentProperty, DateTime dateContentProperty, string occupationContentProperty, string phoneContentProperty, string emailContentProperty, string nationalityContentProperty, List<string> levelContentProperty, List<string> languagesContentProperty, List<string> aptitudesContentProperty, List<string> skillsContentProperty, string profileContentProperty)
+        {
+            InitializeComponent();
+            BindingContext = this;
+            NameContentProperty = nameContentProperty;
+            DateContentProperty = dateContentProperty;
+            OccupationContentProperty = occupationContentProperty;
+            PhoneContentProperty = phoneContentProperty;
+            EmailContentProperty = emailContentProperty;
+            NationalityContentProperty = nationalityContentProperty;
+            LevelContentProperty = new List<string>(levelContentProperty);
+            LanguagesContentProperty = new List<string>(languagesContentProperty);
+            AptitudesContentProperty = new List<string>(aptitudesContentProperty);
+            SkillsContentProperty = new List<string>(skillsContentProperty);
+            ProfileContentProperty = profileContentProperty;
+        }
+    }
+}
